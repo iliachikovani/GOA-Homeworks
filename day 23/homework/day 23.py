5ed531ca1c14b1b56a82d363bcd7b5c34b836fdb
iliachikovani <iliachikovani749@gmail.com>
2024-09-06 19:27:47 +0400

    commit

---
+++ day 23/homework/day 23.py
@@ -0,0 +1,25 @@
+
+hello=["ilia", "davit", "cotne", "aleqsandre"]
+print(hello[1])
+
+# ..................................................
+
+hello = ["ilia", "davit", "cotne", "aleqsandre"]
+hello[1]="gio"
+print(hello)
+
+# ..................................................
+
+mentorebi=["ilia", "davit", "cotne", "aleqsandre", "luka"]
+print(mentorebi[0:2])
+
+# ..................................................
+
+mentorebi=["ilia", "davit", "cotne", "aleqsandre", "luka"]
+print(mentorebi[-5:-3])
+
+# ....................................................
+
+mentorebi=["ilia", "davit", "cotne", "aleqsandre", "luka", "messi"]
+print(mentorebi[0:4])
+
