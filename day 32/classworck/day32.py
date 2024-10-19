4191153fd034f5b50e5e700ae85b6ae9dd8d8a92
iliachikovani <iliachikovani749@gmail.com>
2024-10-19 22:11:46 +0400

    commit

---
+++ day 32/classworck/day32.py
@@ -0,0 +1,21 @@
+
+my_list = list(range(1, 16)) 
+
+middle_elements = my_list[6:9]
+print("Middle elements:", middle_elements)
+
+
+elements_2_to_7 = my_list[2:8]
+print("Elements from index 2 to 7:", elements_2_to_7)
+
+
+elements_start_to_7 = my_list[:7]
+print("Elements from the start to index 7:", elements_start_to_7)
+
+elements_7_to_end = my_list[7:]
+print("Elements from index 7 to the end:",)
+
+# .......................................................................
+
+my_list = ["ilia", "andria","ani","achiko","asmati","gio","luka","aleqsandre","gigson","sandro"]
+
---
+++ day 34/classworck/day34.py
@@ -0,0 +1,19 @@
+# def give_my_wother():
+# print("sheuxvie marjvniv")
+# print("gadadgi 3 nabiji")
+# print("ar waiqce zirs nabiji gaq chasadgmeli")
+# print("moushvi lurj pertan krani")
+# print("ar waiqce magla nabiji gaq chasadgmeli")
+# print("gadadgi 3 nabiji")
+# print("sheuxvie marjvniv")
+# print("daaskanere chemi saxe")
+# print("da tu me var momeci wyali")
+# print("mere bodishi momixade ro daigviane da wadi...")
+
+def svarchikamayvala():
+    num1 = int(input("Enter num1: "))
+    num2 = int(input("Enter num2: "))
+
+
+    print(num1 luwia kentia num2)
+    
